7888d44c2e8d2994a0a9f902e081e7c054a142da
rukkiecodes <rukkiecodes@gmail.com>
2023-03-12 00:51:27 +0100

    pdf sharing works

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import { printToFileAsync } from 'expo-print';
+import { shareAsync } from 'expo-sharing';
 
 export default function App() {
+  const [name, setName] = useState('');
+
+  const html = `
+    <html>
+      <body>
+        <h1>${name}</h1>
+        <p style="color: red;">Some text</p>
+      </body>
+    </html>
+  `;
+
+  let generatePDF = async () => {
+    let { uri } = await printToFileAsync({
+      html,
+      base64: false
+    });
+
+    await shareAsync(uri);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput value={name} placeholder='name' style={styles.textInput} onChangeText={setName} />
+
+      <Button onPress={generatePDF} title='Generate PDF' />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +42,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textInput: {
+    alignSelf: 'stretch',
+    height: 40,
+    margin: 8,
+    padding: 8
+  }
 });
